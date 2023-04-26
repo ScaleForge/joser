2178297c9b89589fc20ce669a956c286e6c3a07e
Alres Arena <alres.arena2019@gmail.com>
2023-04-26 10:59:27 +0800

    feat: implement serialize function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import R from 'ramda';
+
 export type SerializerType<TValue = any, TRaw = any> = {
   type: Function;
   serialize: (value: TValue) => TRaw;
   deserialize: (raw: TRaw) => TValue;
-}
+};
 
 const BUILT_IN_TYPES: SerializerType[] = [
   {
@@ -12,7 +14,7 @@ const BUILT_IN_TYPES: SerializerType[] = [
     },
     deserialize(raw: number) {
       return new Date(raw);
-    }
+    },
   },
   {
     type: Buffer,
@@ -21,23 +23,73 @@ const BUILT_IN_TYPES: SerializerType[] = [
     },
     deserialize(raw: string) {
       return Buffer.from(raw, 'hex');
-    }
-  }
-]
+    },
+  },
+];
 
 export class Serializer {
   private types: Record<string, SerializerType>;
-  constructor(opts?: {
-    types?: SerializerType[]
-  }) {
-    this.types = [...BUILT_IN_TYPES, ...(opts?.types || [])].reduce((acc, item) => {
-      acc[item.type.name] = item;
-      return acc;
-    }, {});
+  constructor(opts?: { types?: SerializerType[] }) {
+    this.types = [...BUILT_IN_TYPES, ...(opts?.types || [])].reduce(
+      (acc, item) => {
+        return {
+          ...acc,
+          [item.type.name]: item,
+        };
+      },
+      {}
+    );
   }
 
+  private getType(value: any) {
+    return R.find(
+      ({ type }) => value instanceof type,
+      [...BUILT_IN_TYPES, ...R.values(this.types)]
+    );
+  }
   serialize(value: any): any {
-    throw new Error('not implemented');
+    if (value === undefined) {
+      return undefined;
+    }
+
+    if (value === null) {
+      return null;
+    }
+
+    const _type = typeof value;
+
+    if (_type === 'number' || _type === 'string' || _type === 'boolean') {
+      return value;
+    }
+
+    if (value instanceof Array) {
+      return R.map((v) => this.serialize(v), value);
+    }
+
+    const type = this.getType(value);
+
+    if (!type) {
+      const data: Record<string, unknown> = {};
+      const __t: Record<string, unknown> = {};
+      for (const [key, val] of R.toPairs(value)) {
+        data[key] = this.serialize(val);
+
+        const type = this.getType(val);
+
+        if (type) {
+          __t[key] = type.type.name;
+        }
+      }
+
+      return {
+        ...data,
+        __t: {
+          ...__t,
+        },
+      };
+    }
+
+    return type.serialize(value as never);
   }
 
   deserialize<T>(raw: any): T {
