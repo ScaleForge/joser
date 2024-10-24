24b0f08c295c536c0f2ad516f79f7bf52357803b
ryacunas <86036254+yacunas@users.noreply.github.com>
2024-10-24 17:09:29 +0800

    add support for serializing nested array

--- src/index.test.ts
+++ src/index.test.ts
@@ -55,12 +55,63 @@ describe('Joser', () => {
     ],
     [
       {
+        Array: [
+          Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+          new Date('2023-04-25T00:00:00Z'),
+          'string',
+          true,
+          null,
+          [
+            'string',
+            true,
+            null,
+            Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            new Date('2023-04-25T00:00:00Z'),
+          ],
+        ],
+      },
+      {
+        Array: [
+          'AAECAwQFBgcICQ==',
+          1682380800000,
+          'string',
+          true,
+          null,
+          ['string', true, null, 'AAECAwQFBgcICQ==', 1682380800000],
+        ],
+        __t: {
+          t: ['Buffer', 'Date'],
+          i: {
+            Array: [
+              [0, 0],
+              [1, 1],
+              [
+                5,
+                [
+                  [3, 0],
+                  [4, 1],
+                ],
+              ],
+            ],
+          },
+        },
+      },
+    ],
+    [
+      {
         number: 1,
         string: 'string',
         boolean: true,
         null: null,
         Date: new Date('2023-04-25T00:00:00Z'),
         Buffer: Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        Array: [
+          Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+          new Date('2023-04-25T00:00:00Z'),
+          'string',
+          true,
+          null,
+        ],
         Object: {
           number: 1,
           string: 'string',
@@ -75,6 +126,20 @@ describe('Joser', () => {
             null: null,
             Date: new Date('2023-04-25T00:00:00Z'),
             Buffer: Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            Array: [
+              'string',
+              true,
+              Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+              new Date('2023-04-25T00:00:00Z'),
+              null,
+              [
+                'string',
+                true,
+                Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+                new Date('2023-04-25T00:00:00Z'),
+                null,
+              ],
+            ],
           },
         },
       },
@@ -85,6 +150,7 @@ describe('Joser', () => {
         null: null,
         Date: 1682380800000,
         Buffer: 'AAECAwQFBgcICQ==',
+        Array: ['AAECAwQFBgcICQ==', 1682380800000, 'string', true, null],
         Object: {
           number: 1,
           string: 'string',
@@ -99,6 +165,14 @@ describe('Joser', () => {
             null: null,
             Date: 1682380800000,
             Buffer: 'AAECAwQFBgcICQ==',
+            Array: [
+              'string',
+              true,
+              'AAECAwQFBgcICQ==',
+              1682380800000,
+              null,
+              ['string', true, 'AAECAwQFBgcICQ==', 1682380800000, null],
+            ],
           },
         },
         __t: {
@@ -106,19 +180,34 @@ describe('Joser', () => {
           i: {
             Date: 0,
             Buffer: 1,
+            Array: [
+              [0, 1],
+              [1, 0],
+            ],
             Object: {
               Date: 0,
               Buffer: 1,
               Object: {
                 Date: 0,
                 Buffer: 1,
+                Array: [
+                  [2, 1],
+                  [3, 0],
+                  [
+                    5,
+                    [
+                      [2, 1],
+                      [3, 0],
+                    ],
+                  ],
+                ],
               },
             },
           },
         },
       },
     ],
-  ] as [never, never ][];
+  ] as [never, never][];
 
   test.each(cases)('serialize %p', (obj, expected) => {
     expect(new Joser().serialize(obj)).toEqual(expected);
